e77f09b6bf8ec210debb0edb45bad8f1921bf77a
Merlijn <47323359+merlijnzeijden@users.noreply.github.com>
2023-10-30 11:36:25 +0100

    Removed deprecated runList command

--- src/commands.ts
+++ src/commands.ts
@@ -23,30 +23,6 @@ export function runCompiler(context: vscode.ExtensionContext){
     }
 }
 
-export function runList(context: vscode.ExtensionContext){
-    let cwd = vscode.workspace.getConfiguration('pxplus').get<string>('workingDirectory.programs');
-    let cmd = vscode.workspace.getConfiguration('pxplus').get<string>('commands.lst');
-	let t = selectTerminal();
-
-    t.show();
-    if(cwd){
-        if(cmd === ""){
-            t.sendText('ECHO "You need to configure your list command first."');
-            return;
-        }
-        
-        t.sendText("cd" + " " + cwd);
-
-        cmd = cmd?.replace("%f", vscode.window.activeTextEditor?.document.fileName || "");
-        if(cmd){
-            t.sendText(cmd);
-        }
-    }else{
-        t.sendText('ECHO "You need to configure your working directory first."');
-    }
-}
-
-
 function selectTerminal(): vscode.Terminal {
     ensureTerminalExists();
 	return vscode.window.terminals.filter((t)=>{
