5021d3c92327e136fa06b60b17b767d2d0994fab
Kevin Kremers <blado69@gmail.com>
2023-10-31 09:25:16 +0100

    Compile program

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,24 @@
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+const path = require('path');
 
 export function runCompiler(context: vscode.ExtensionContext){
+    let documentFileName = vscode.window.activeTextEditor?.document.fileName;
+    let fileName = path.basename(documentFileName);
+
     let pxplusPath = vscode.workspace.getConfiguration('pxplus').get<string>('pxplusDirectory.path');
-    let isWindx = vscode.workspace.getConfiguration('pxplus').get<boolean>('windx.client');
+    let compiler = vscode.workspace.getConfiguration('pxplus').get<string>('pxplusDirectory.program');
+    let isWindx = (vscode.workspace.getConfiguration('pxplus').get<boolean>('windx.client') ?? false);
+
+    let sourceProgram = documentFileName;
+    let compileProgram = vscode.workspace.getConfiguration('pxplus').get<string>('workingDirectory.program') + fileName.replace(/.pxprg$/, '');
+
+    if (isWindx) {
+    //let sourceProgram = vscode.workspace.getConfiguration('pxplus').get<string>('workingDirectory.source') + fileName;
+    //let compileProgram = vscode.workspace.getConfiguration('pxplus').get<string>('workingDirectory.program') + fileName;
+    }
+
+    let errorPath = vscode.workspace.getConfiguration('pxplus').get<string>('workingDirectory.errors');
     
     // Select a terminal
     let terminal = selectTerminal();
@@ -11,7 +27,7 @@ export function runCompiler(context: vscode.ExtensionContext){
     terminal.show();
 
     // Send a command to the terminal (as text)
-    terminal.sendText("echo hallo");
+    terminal.sendText('"' + pxplusPath + ' ' + compiler + ' -arg '+ sourceProgram + ' ' + compileProgram +' '+ errorPath + '"');
 }
 
 function selectTerminal(): vscode.Terminal {
