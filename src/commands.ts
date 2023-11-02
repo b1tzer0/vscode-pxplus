4e3ec3c23abbed151c805524320d887130ea580f
Merlijn <47323359+merlijnzeijden@users.noreply.github.com>
2023-11-02 09:24:13 +0100

    Improved getConfiguration function

--- src/commands.ts
+++ src/commands.ts
@@ -14,19 +14,19 @@ export function runCompiler(context: vscode.ExtensionContext) {
     const fileName = path.basename(documentFileName);
     const fileNameWithoutExtension = fileName.replace(/.pxprg$/, '');
 
-    const pxplusPath = normalizeToDoubleBackslash(getConfiguration('pxplusDirectory.path'));
-    const compiler = normalizeToSingleBackslash(getConfiguration('pxplusDirectory.compiler'));
-    const isWindx = getConfiguration('windx.client', 'false') === 'true';
+    const pxplusPath = normalizeToDoubleBackslash(getConfiguration<string>('pxplusDirectory.path', ''));
+    const compiler = normalizeToSingleBackslash(getConfiguration<string>('pxplusDirectory.compiler', ''));
+    const isWindx = getConfiguration<boolean>('windx.client', false);
 
     let sourceProgram = documentFileName;
-    let compileProgramPath = normalizeToSingleBackslash(getConfiguration('workingDirectory.output')).replace("%f", filePath);
+    let compileProgramPath = normalizeToSingleBackslash(getConfiguration<string>('workingDirectory.output', '')).replace("%f", filePath);
     let compileProgram = path.join(compileProgramPath, fileNameWithoutExtension);
 
     if (isWindx) {
         // Adjust sourceProgram and compileProgram here if needed for WindX
     }
 
-    const errorPath = normalizeToSingleBackslash(getConfiguration('workingDirectory.errors'));
+    const errorPath = normalizeToSingleBackslash(getConfiguration<string>('workingDirectory.errors', ''));
 
     // Select a terminal
     const terminal = selectTerminal();
@@ -39,8 +39,8 @@ export function runCompiler(context: vscode.ExtensionContext) {
     terminal.sendText(command);
 }
 
-function getConfiguration(key: string, defaultVal: string = ''): string {
-    return vscode.workspace.getConfiguration('pxplus').get<string>(key) ?? defaultVal;
+function getConfiguration<T>(key: string, defaultVal: T): T {
+    return vscode.workspace.getConfiguration('pxplus').get<T>(key) ?? defaultVal;
 }
 
 function normalizeToSingleBackslash(path: string): string {
